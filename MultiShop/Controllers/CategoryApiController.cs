08f39f1a1f0c40cd4e2d2a1284186c8fbce6568d
Muhammad-Bilal <meharbilal24525@gmail.com>
2022-07-19 18:47:17 +0500

    Category Web Api Controllers Update

---
+++ MultiShop/Controllers/CategoryApiController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DataAccess.Infrastructure.IRepository;
+using MultiShop.DataAccess.Infrastructure.Repository;
+using MultiShop.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultiShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryApiController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryApiController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                return Ok(await _categoryRepository.GetAllCategory());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error\nError While Retreiving Categories From Server");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Category>> GetCategoryById(int id)
+        {
+            try
+            {
+                var result = await _categoryRepository.GetCategoryById(id);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,"Internal Server Error");
+            }
+
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Category>> CreateCategory(Category category)
+        {
+            try
+            {
+                if(category==null)
+                {
+                    return BadRequest();
+                }
+
+                var createCategory = await _categoryRepository.CreateCategory(category);
+                return CreatedAtAction(nameof(GetCategoryById), new { id = createCategory.Id }, createCategory);
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error While Creating New Category");
+            }
+        }
+
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<Category>> UpdateCategoryById(int id,Category category)
+        {
+            try
+            {
+                if (id != category.Id)
+                {
+                    return BadRequest("Requewsting Category With id is Not Found So It's Can't be Updated");
+                }
+                var result = await _categoryRepository.GetCategoryById(id);
+                if(result==null)
+                {
+                    return NotFound($"Category With Requesting Details Like ID : {id} not found");
+                }
+
+                return await _categoryRepository.UpdateCategory(category);
+
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error While Updating Data at Current ID");
+            }
+        }
+
+
+        [HttpDelete("{id:int}")]
+
+        public async Task<ActionResult> DeleteCategoryById(int id)
+        {
+            try
+            {
+                var result = await _categoryRepository.GetCategoryById(id);
+                if (result == null)
+                {
+                    return NotFound($"Category With Requesting Details (Category Id: {id}) to Delete Is not found");
+                }
+                await _categoryRepository.DeleteCategoryById(id);
+                return Ok($"Category With ID : {id} is Delete Successfully");
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error Deleting Category");
+            }
+        }
+
+
+
+
+    }
+}
