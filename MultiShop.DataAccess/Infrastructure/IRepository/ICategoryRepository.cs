10b0b811c7a87e47d6f901e0a20ecf3871d47df7
Muhammad-Bilal <meharbilal24525@gmail.com>
2022-07-19 15:57:22 +0500

    Category Repository WorkDone

---
+++ MultiShop.DataAccess/Infrastructure/IRepository/ICategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using MultiShop.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.DataAccess.Infrastructure.IRepository
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllCategory();
+        Task<Category> GetCategoryById(int id);
+        Task<Category> CreateCategory(Category category);
+        Task DeleteCategoryById(int id);
+        Task<Category> UpdateCategory(Category category);
+
+
+    }
+}
---
+++ MultiShop.DataAccess/Infrastructure/Repository/CategoryRepository.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.DataAccess.Data;
+using MultiShop.DataAccess.Infrastructure.IRepository;
+using MultiShop.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.DataAccess.Infrastructure.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IEnumerable<Category>> GetAllCategory()
+        {
+            return await _context.Category.ToListAsync();
+        }
+
+        public async Task<Category> GetCategoryById(int id)
+        {
+            var result = await _context.Category.FirstOrDefaultAsync(x => x.Id == id);
+            if (result != null)
+            {
+                return result;
+            }
+            return null;
+        }
+        
+        
+        public async Task<Category> CreateCategory(Category category)
+        {
+            var result=await _context.Category.AddAsync(category);
+            await _context.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        
+        public async Task DeleteCategoryById(int id)
+        {
+
+            var result = await _context.Category.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(result != null)
+            {
+                _context.Category.Remove(result);
+                await _context.SaveChangesAsync();
+            }
+        
+        }
+        public async Task<Category> UpdateCategory(Category category)
+        {
+            var result = await _context.Category.FirstOrDefaultAsync(x => x.Id == category.Id);
+            if (result != null)
+            {
+                result.Id = category.Id;
+                result.Name = category.Name;
+                await _context.SaveChangesAsync();
+                return result;
+            }
+            
+            return null;
+            
+        }
+    }
+}
