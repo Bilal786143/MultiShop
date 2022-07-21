f50e8ace084f1f1edc3d6836022a2a3425341527
Muhammad-Bilal <meharbilal24525@gmail.com>
2022-07-21 17:28:37 +0500

    Modify Admin Controller

--- MultiShop/Controllers/AdminApiController.cs
+++ MultiShop/Controllers/AdminApiController.cs
@@ -13,111 +13,100 @@ namespace MultiShop.Controllers
     [ApiController]
     public class AdminApiController : ControllerBase
     {
-        
-            private readonly IAdminRepository _admin;
-
-            public AdminApiController(IAdminRepository admin)
+        private readonly IAdminRepository _admin;
+        public AdminApiController(IAdminRepository admin)
             {
                 _admin = admin;
             }
-            [HttpGet]
-            public async Task<IActionResult> GetAdminList()
+        
+        [HttpGet]
+        public async Task<IActionResult> GetAdminList()
+        {
+            try
             {
-                try
-                {
-                    return Ok(await _admin.GetAllAdmin());
-                }
-                catch (Exception)
-                {
-
-                    return StatusCode(StatusCodes.Status404NotFound, "Content Not Found ");
-                }
-
+                return Ok(await _admin.GetAllAdmin());
             }
-            [HttpGet("{ id:int}")]
-            public async Task<IActionResult> GetAdminById(int id)
+            catch (Exception)
             {
-                try
+                return StatusCode(StatusCodes.Status404NotFound, "Content Not Found ");
+            }
+        }
+        
+        [HttpGet("{ id:int}")]
+        public async Task<IActionResult> GetAdminById(int id)
+        {
+            try
+            {
+                if (id == 0)
                 {
-                    if (id == 0)
-                    {
-                        return BadRequest($"This ID {id} Could not found please enter a valid ID");
-                    }
+                    return BadRequest($"This ID {id} Could not found please enter a valid ID");
+                }
                     else return Ok(await _admin.GetAdminById(id));
-
-                }
-                catch (Exception)
-                {
-
-                    return StatusCode(StatusCodes.Status500InternalServerError, "Server is not responding ");
-                }
             }
-            [HttpPost]
-            public async Task<IActionResult> CreateAdmin(Admin admin )
+            catch (Exception)
             {
-                try
-                {
-                    if (admin == null)
-                    {
-                        return NotFound();
-                    }
-                    else return
-                   Ok(await _admin.CreateAdmin(admin));
-                }
-                catch (Exception)
-                {
-
-                    return StatusCode(StatusCodes.Status500InternalServerError, "Server is Not Rresponding");
-                }
-
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server is not responding ");
             }
-            [HttpPost]
-            public async Task<IActionResult> EditAdmin(Admin admin)
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> CreateAdmin(Admin admin)
+        {
+            try
             {
-                try
+                if (admin == null)
                 {
-                    if (admin == null)
-                    {
-                        return NotFound();
-
-                    }
-                    else
-                    {
-                        return Ok(await _admin.EditAdmin(admin));
-                    }
+                    return BadRequest();
                 }
-                catch (Exception)
-                {
-
-                    return StatusCode(StatusCodes.Status500InternalServerError, "Server is not responding !!!");
-                }
-
-
+                var createAdmin = await _admin.CreateAdmin(admin);
+                return CreatedAtAction(nameof(GetAdminById), new { id = createAdmin.Id }, createAdmin);
             }
-            [HttpDelete("{ id:int}")]
-            public IActionResult DeleteAdmin(int id)
+            catch (Exception)
             {
-                try
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server is Not Rresponding");
+            }
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> EditAdmin(Admin admin)
+        {
+            try
+            {
+                if (admin == null)
                 {
-
-                    var result =  _admin.GetAdminById(id);
-                    if (result != null)
-                    {
-
-                        return Ok(_admin.DeleteAdmin(id));
-                    }
                     return NotFound();
-
-
                 }
-                catch (Exception)
+                else
                 {
-                    return StatusCode(StatusCodes.Status500InternalServerError, "Server is not responding !!!");
-
+                    return Ok(await _admin.EditAdmin(admin));
                 }
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server is not responding !!!");
+            }
+        }
+        
+        [HttpDelete("{ id:int}")]
+        public IActionResult DeleteAdmin(int id)
+        {
+            try
+            {
+                var result =  _admin.GetAdminById(id);
+                if (result != null)
+                {
+                    return Ok(_admin.DeleteAdmin(id));
+                }
+                return NotFound();
+            
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server is not responding !!!");
             }
         }
     }
+}
 
 
 
