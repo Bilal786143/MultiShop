f4d233db584e7bf8fd5357b70b901f7f3af37532
Muhammad-Bilal <meharbilal24525@gmail.com>
2022-07-30 15:57:58 +0500

    Modify Order Table and Create Order Details Table

---
+++ MultiShop.Models/Models/OrderDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Models.Models
+{
+    public class OrderDetails
+    {
+        public int Id { get; set; }
+        public int ProductQuantity { get; set; }
+        public double SalePrice { get; set; }
+        public double TotalPrice { get; set; }
+
+        public int ProductFId { get; set; }
+        [ForeignKey("ProductFId")]
+        public Product Product { get; set; }
+        public int OrderFId { get; set; }
+        [ForeignKey("OrderFId")]
+        public Order Order { get; set; }
+
+    }
+}
