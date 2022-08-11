1c6437e611dbc0b75357e82acf5900f479993b20
Usama <usamanasir464@gmail.com>
2022-08-11 04:08:12 -0700

    change

--- MultiShop.Models/Models/DTOs/CartDetailsDto.cs
+++ MultiShop.Models/Models/DTOs/CartDetailsDto.cs
@@ -4,20 +4,10 @@ namespace MultiShop.Models.Models.DTOs
 {
     public class CartDetailsDto
     {
-        [Key]
+      
         public int CartDetailsId { get; set; }
-       
         public int CartHeaderFId { get; set; }
-        [ForeignKey("CartHeaderFId")]
-        public virtual CartHeaderDto CartHeader { get; set; }
         public int ProductFId { get; set; }
-        [ForeignKey("ProductFId")]
-        public virtual ProductDto Product { get; set; }
        public int Count { get; set; }
-
-
-
-        //public int ProductQuantity { get; set; }
-        //public decimal TotalPrice { get; set; 
     }
 }
