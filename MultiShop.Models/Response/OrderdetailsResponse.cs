fa51283387710422e59fdd32c0bdbd99e1dd0ab3
Usama <usamanasir464@gmail.com>
2022-08-22 02:39:38 -0700

    CHange

--- MultiShop.Models/Response/OrderdetailsResponse.cs
+++ MultiShop.Models/Response/OrderdetailsResponse.cs
@@ -1,13 +1,8 @@
 ﻿using MultiShop.Models.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MultiShop.Models.Response
 {
-  public  class OrderdetailsResponse
+    public  class OrderdetailsResponse
     {
         public OrderDetails OrderDetails { get; set; }
     }
